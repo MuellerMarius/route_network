30c39e9ab1061277ca0af3a4caeb188b2bae733b
MuellerMarius <marius_m@gmx.net>
2020-04-22 12:18:04 +0200

    Troubleshooting

--- api/controllers/airports.js
+++ api/controllers/airports.js
@@ -17,41 +17,9 @@ let data = [];
 //     console.log('Database loaded');
 //   });
 
-function readFiles(pathName) {
-  var fileNames = '';
-  fs.readdir(pathName, (err, files) => {
-    //handling error
-    if (err) {
-      return console.log('Unable to scan directory: ' + err);
-    }
-    //listing all files using forEach
-    files.forEach((file) => (fileNames = fileNames.concat(file)));
-  });
-  console.log(fileNames);
-  return fileNames;
-}
-
 exports.getAirportData = (req, res, next) => {
-  console.log('current');
-  fs.readdir(path.join(__dirname, '.'), (err, files) => {
-    //handling error
-    if (err) {
-      return console.log('Unable to scan directory: ' + err);
-    }
-    //listing all files using forEach
-    console.log(files);
-  });
-  console.log('parent');
-  fs.readdir(path.join(__dirname, '../'), (err, files) => {
-    //handling error
-    if (err) {
-      return console.log('Unable to scan directory: ' + err);
-    }
-    //listing all files using forEach
-    console.log(files);
-  });
-  console.log('lambda root');
   fs.readdir(path.join(process.env.LAMBDA_TASK_ROOT, '.'), (err, files) => {
+    console.log('lambda root');
     //handling error
     if (err) {
       return console.log('Unable to scan directory: ' + err);
@@ -59,15 +27,19 @@ exports.getAirportData = (req, res, next) => {
     //listing all files using forEach
     console.log(files);
   });
-  console.log('lambda root parent');
-  fs.readdir(path.join(process.env.LAMBDA_TASK_ROOT, '../'), (err, files) => {
-    //handling error
-    if (err) {
-      return console.log('Unable to scan directory: ' + err);
+
+  fs.readdir(
+    path.join(process.env.LAMBDA_TASK_ROOT, '../db/'),
+    (err, files) => {
+      console.log('db');
+      //handling error
+      if (err) {
+        return console.log('Unable to scan directory: ' + err);
+      }
+      //listing all files using forEach
+      console.log(files);
     }
-    //listing all files using forEach
-    console.log(files);
-  });
+  );
 
   return res.status(200).json({
     dirname: __dirname,
