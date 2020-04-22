05cd91aabf3a4d3157b49310b783ffb6a5ce7c4a
MuellerMarius <marius_m@gmx.net>
2020-04-22 15:38:50 +0200

    Another try

--- functions/api/api.js
+++ functions/api/api.js
@@ -2,22 +2,21 @@ const fs = require('fs');
 const path = require('path');
 const csv = require('csv-parser');
 
-let data = [];
-fs.createReadStream(path.join(process.env.LAMBDA_TASK_ROOT, 'db.csv'))
-  .pipe(
-    csv({
-      separator: ';',
-      raw: false,
-    })
-  )
-  .on('data', function (row) {
-    data.push(row);
-  })
-  .on('end', function () {
-    console.log('Database loaded');
-  });
-
 exports.handler = async (event, context, callback) => {
+  let data = [];
+  fs.createReadStream(path.join(process.env.LAMBDA_TASK_ROOT, 'db.csv'))
+    .pipe(
+      csv({
+        separator: ';',
+        raw: false,
+      })
+    )
+    .on('data', function (row) {
+      data.push(row);
+    })
+    .on('end', function () {
+      console.log('Database loaded');
+    });
   const airportCode = event.queryStringParameters.icao;
   console.log(`querying ${airportCode}`);
   const answer = data.find((el) => el.ident === airportCode);
