084e75fd51a5286fda3ac2a9662b9768c6e1bdc7
MuellerMarius <marius_m@gmx.net>
2020-08-07 13:22:48 +0000

    Fixed path to db

--- functions/server/controllers/airports.js
+++ functions/server/controllers/airports.js
@@ -2,57 +2,42 @@ const fs = require('fs');
 const csv = require('csv-parser');
 const path = require('path');
 
-const logdir = (dir) => {
-  fs.readdir(dir, function (err, files) {
-    //handling error
-    if (err) {
-      return console.log('Unable to scan directory: ' + err);
-    }
-    //listing all files using forEach
-    console.log(`LOGGING DIR: ${dir}`);
-    files.forEach(function (file) {
-      // Do whatever you want to do with the file
-      console.log(file);
-    });
-  });
-};
-
 exports.getAirportData = (req, res, next) => {
   let data = [];
 
-  logdir(path.join(process.env.LAMBDA_TASK_ROOT, 'src', 'functions'));
-
-  logdir(path.join(process.env.LAMBDA_TASK_ROOT, 'src', 'functions', 'server'));
-  logdir(
-    path.join(
-      process.env.LAMBDA_TASK_ROOT,
-      'src',
-      'functions',
-      'server',
-      'database'
-    )
+  // local path
+  const dbPathLocal = path.join(
+    process.env.LAMBDA_TASK_ROOT,
+    'database',
+    'db.csv'
   );
 
-  return res.status(200).json('{"test": "gut"}');
+  // netlify path
+  const dbPath = path.join(
+    process.env.LAMBDA_TASK_ROOT,
+    'src',
+    'functions',
+    'server',
+    'database',
+    'db.csv'
+  );
 
-  // fs.createReadStream(
-  //   path.join(process.env.LAMBDA_TASK_ROOT, 'database', 'db.csv')
-  // )
-  //   .pipe(
-  //     csv({
-  //       separator: ';',
-  //       raw: false,
-  //     })
-  //   )
-  //   .on('data', function (row) {
-  //     data.push(row);
-  //   })
-  //   .on('end', function () {
-  //     const requestedData = data.find((el) => el.ident === req.params.id);
-  //     if (requestedData) {
-  //       return res.status(200).json(requestedData);
-  //     } else {
-  //       return res.status(404).send(`${req.params.id} could not be found.`);
-  //     }
-  //   });
+  fs.createReadStream(dbPath)
+    .pipe(
+      csv({
+        separator: ';',
+        raw: false,
+      })
+    )
+    .on('data', function (row) {
+      data.push(row);
+    })
+    .on('end', function () {
+      const requestedData = data.find((el) => el.ident === req.params.id);
+      if (requestedData) {
+        return res.status(200).json(requestedData);
+      } else {
+        return res.status(404).send(`${req.params.id} could not be found.`);
+      }
+    });
 };
