02fb067a7ee327c16eff9d5f15ae87c7a7e2d5d5
Marius Müller <60852282+MuellerMarius@users.noreply.github.com>
2020-09-23 06:10:18 +0200

    Removes unused import

--- client/src/components/MapContainer/MapView/index.js
+++ client/src/components/MapContainer/MapView/index.js
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from 'react';
+import React, { useContext } from 'react';
 import PropTypes from 'prop-types';
 import { ComposableMap, ZoomableGroup } from 'react-simple-maps';
 import { GlobalContext } from '../../../context/GlobalState';
